0680918cebbaf6d6a965520f3fdd669b2a67f53a
Mstensk <mathias_stensler@live.dk>
2021-11-25 17:04:46 +0100

    champions implementation done

--- src/main/java/kea/lhk/rito_api/controllers/Champions.java
+++ src/main/java/kea/lhk/rito_api/controllers/Champions.java
@@ -12,8 +12,10 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.w3c.dom.NodeList;
 
 import java.io.IOException;
 import java.net.URL;
@@ -31,17 +33,33 @@ public class Champions {
     public String importSummonersIntoDatabase(){
 
 
-        String docChamps = null;
-        try {
-            docChamps = Jsoup.connect(champions.url).ignoreContentType(true).get().toString();
 
-            for(int b = 0; b<30; b++) {
+        try {
+            String docChamps = Jsoup.connect(champions.url).ignoreContentType(true).get().toString();
+            int count= StringUtils.countOccurrencesOf(docChamps,"name\"");
+
+            for(int i = 0; i < count; i++) {
+
                 Champion champion = new Champion();
 
                 String champName = docChamps.substring(docChamps.indexOf("\"name\":")+8,
                         docChamps.indexOf("\"title\":")-2);
+                champion.setChampionName(champName);
 
-                System.out.println(champName);
+                String champId = docChamps.substring(docChamps.indexOf("\"key\":")+7,docChamps.indexOf(",\"name\":")-1);
+                champion.setChampionId(champId);
+
+                String champTitle = docChamps.substring(docChamps.indexOf("\"title\":")+9,
+                        docChamps.indexOf(",\"blurb\":")-1);
+                champion.setChampionTitle(champTitle);
+
+               /* String champDesc = docChamps.substring(docChamps.indexOf("\"blurb\":")+9,
+                        docChamps.indexOf(",\"info\":")-1);
+                champion.setChampionDesc(champDesc); */
+
+                docChamps = docChamps.substring(docChamps.indexOf("\"info\":")+10);
+                champions.save(champion);
+
 
             }
         } catch (IOException e) {
