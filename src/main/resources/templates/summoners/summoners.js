0d3c9ff92fb1287e6ce93459201b4a6b151a9640
Lasse <knoefler12>
2021-11-26 16:43:16 +0100

    kdd

--- src/main/resources/templates/summoners/summoners.js
+++ src/main/resources/templates/summoners/summoners.js
@@ -1,3 +1,18 @@
+const queryString = window.location.search;
+const URLParams = new URLSearchParams(queryString);
+const summonerId = URLParams.get("summonerId");
+
+fetch(baseURL + "/summoners/" + summonerId)
+    .then(response => response.json())
+    .then(summoner => {
+        document.getElementById("summoner-name").innerText = summoner.name;
+        document.getElementById("summoner-account-id").innerText = summoner.accountId;
+        document.getElementById("summoner-profile-icon-id").innerText = summoner.profileIconId;
+        document.getElementById("summoner-puuid").innerText = summoner.puuid;
+        document.getElementById("summoner-revision-date").innerText = summoner.revisionDate;
+        document.getElementById("summoner-level").innerText = summoner.level;
+    });
+
 
 fetch(baseURL)
 .then(respones => respones.json)
